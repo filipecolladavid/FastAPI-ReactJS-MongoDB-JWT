7696633482f8e0dd273d03bc758f11b0330032ed
Filipe Colla David <filipecolladavid@gmail.com>
2023-03-01 13:26:02 +0000

    Removed other MINIO references

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -12,10 +12,5 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str = os.environ["JWT_ALGORITHM"]
 
     CLIENT_ORIGIN: str = os.environ["CLIENT_ORIGIN"]
-
-    MINIO_URL: str = os.environ["MINIO_URL"]
-    MINIO_ROOT_USER: str = os.environ["MINIO_ROOT_USER"]
-    MINIO_ROOT_PASSWORD: str = os.environ["MINIO_ROOT_PASSWORD"]
-    MINIO_SECURE: bool = os.environ["MINIO_SECURE"]
         
 settings = Settings()
